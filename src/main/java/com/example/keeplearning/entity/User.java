51b214e4fb4d11999d9d2d40fc06f35e1b9f05e4
jdjennewein <117037223+jdjennewein@users.noreply.github.com>
2025-12-18 15:12:45 +0100

    Kommentare verändert

--- src/main/java/com/example/keeplearning/entity/User.java
+++ src/main/java/com/example/keeplearning/entity/User.java
@@ -80,18 +80,6 @@ public class User implements UserDetails {
     //////////////////////////////////////////////////////////////////////////
     //// UserDetails implementation
 
-    /*@Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new Authority(role));
-        return authorities;
-    }*/
-    /*@Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(
-                new SimpleGrantedAuthority("ROLE_" + role.toUpperCase())
-        );
-    }*/
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
